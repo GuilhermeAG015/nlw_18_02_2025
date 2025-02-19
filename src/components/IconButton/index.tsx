15e8ebf0906482a1e9b15d75ddf938709f16ac83
Guilherme Alexandre <guilhermegrilo2009@hotmail.com>
2025-02-19 20:34:29 -0300

    Styles adjustments

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -4,7 +4,7 @@ interface IconButtonProps extends ComponentProps<'button'>{}
 export function IconButton(props: IconButtonProps) {
   return (
     <button 
-      className="p-1.5 bg-gray-500 text-blue rounded-md cursor-point transition-colors duration-300 hover:bg-blue hover:text-gray-900"
+      className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900"
       {...props}
     />
   )
