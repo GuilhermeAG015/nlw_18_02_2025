351beb080a47086809c19579d7c29d4dcf28fc96
Guilherme Alexandre <guilhermegrilo2009@hotmail.com>
2025-02-21 21:12:12 -0300

    Adding cpliboard.WriteText

--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -5,6 +5,8 @@ import { Ranking } from './SubComponents/Ranking'
 import { Stats } from './SubComponents/Stats'
 
 export default function InvitePage() {
+  const inviteLink = 'http://localhost:3000/invite/165465123aad6548'
+
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className="flex flex-col gap-10 w-full max-w-[550px]">
@@ -31,7 +33,7 @@ export default function InvitePage() {
             </p>
           </div>
 
-          <InviteLinkInput />
+          <InviteLinkInput inviteLink={inviteLink} />
           <Stats />
         </div>
       </div>
